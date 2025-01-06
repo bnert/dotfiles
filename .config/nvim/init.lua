7397b4a8f51adc12018e8bfd29789c2920d709dd
Brent Soles <brent.soles@vannevarlabs.com>
2025-01-06 09:57:56 -0500

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -117,7 +117,11 @@ require("lazy").setup({
     end
   },
   {
-    "rose-pine/neovim", name = "rose-pine-dawn"
+    "rose-pine/neovim",
+    name = "rose-pine-dawn",
+    config = function()
+      vim.cmd("colorscheme rose-pine-dawn")
+    end
   },
   -- {
   --   "catppuccin/nvim",
@@ -257,6 +261,53 @@ require("lazy").setup({
     end,
     dependencies = { "williamboman/mason.nvim" }
   },
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      use_icons = false
+    },
+    keys = {
+      {
+        "<leader>dvm",
+        "<cmd>DiffviewOpen origin/main...HEAD<cr>",
+        desc = "Open diff against main"
+      },
+    }
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = {
+      on_attach = function(client, bufnr)
+
+        if client.supports_method("textDocument/formatting") then
+          vim.keymap.set("n", "<leader>pf", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+          end, { buffer = bufnr, desc = "[lsp] format" })
+
+          -- format on save
+          local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+          local event = "BufWritePre"
+          local async = event == "BufWritePost"
+
+          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+          vim.api.nvim_create_autocmd(event, {
+            buffer = bufnr,
+            group = group,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr, async = async })
+            end,
+            desc = "[lsp] format on save",
+          })
+        end
+
+        if client.supports_method("textDocument/rangeFormatting") then
+          vim.keymap.set("x", "<leader>pf", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+          end, { buffer = bufnr, desc = "[lsp] format" })
+        end
+      end
+    }
+  }
 })
 
 require("rainbow-delimiters")

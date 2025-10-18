f04b7834035f4bb4139f97591c699591bf7b9c23
Brent Soles (@bnert) <17535379+bnert@users.noreply.github.com>
2025-10-18 11:38:04 -0400

    Simplify nvim

--- .config/nvim/bak/plugins.bak/treesitter.lua
+++ .config/nvim/bak/plugins.bak/treesitter.lua
@@ -1,7 +1,18 @@
--- [nfnl] fnl/plugins/treesitter.fnl
-local trees = {"clojure", "fennel", "go", "graphql", "javascript", "html", "python", "typescript", "haskell", "sql", "zig"}
+local trees = {
+  "clojure",
+  "go",
+  "javascript",
+  "html",
+  "python",
+  "typescript",
+  "haskell",
+  "sql",
+  "zig"
+}
+
 local function config()
   local configs = require("nvim-treesitter.configs")
   return configs.setup({ensure_installed = trees, highlight = {enable = true}, indent = {enable = true}, sync_install = false})
 end
+
 return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = config}
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,68 +1,66 @@
-_G.vim.g.mapleader = ";"
-_G.vim.g.maplocalleader = ";"
-_G.vim.o.path = ".,,**"
-_G.vim.o.wildignore = "**/node_modules/,**/__pycache__/"
-_G.vim.o.number = true
-_G.vim.o.list = true
-_G.vim.o.listchars = "tab:\226\141\191 ,trail:\194\183"
-_G.vim.o.colorcolumn = "80"
-_G.vim.o.syntax = "on"
-_G.vim.o.cursorline = true
-_G.vim.o.ttyfast = true
-_G.vim.o.list = true
-_G.vim.o.expandtab = true
-_G.vim.o.autoindent = true
-_G.vim.o.tabstop = 2
-_G.vim.o.softtabstop = 2
-_G.vim.o.shiftwidth = 2
-_G.vim.wo.wrap = true
-_G.vim.diagnostic.config = {virtual_lines = {current_line = true}}
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ";"
+vim.o.path = ".,,**"
+vim.o.wildignore = "**/node_modules/,**/__pycache__/"
+vim.o.number = true
+vim.o.list = true
+vim.o.listchars = "tab:\226\141\191 ,trail:\194\183"
+vim.o.colorcolumn = "80"
+vim.o.syntax = "off"
+vim.o.cursorline = true
+vim.o.ttyfast = true
+vim.o.list = true
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.wo.wrap = true
+vim.diagnostic.config = {virtual_lines = {current_line = true}}
 
-_G.vim.o.termguicolors = true
-_G.vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-_G.vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-_G.vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-_G.vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
-_G.vim.o.background = "light"
+vim.o.termguicolors = true
+vim.o.background = "light"
 
-require("config.lazy")
-
-_G.vim.lsp.config(
-  "*",
-  {
-    capabilities = _G.vim.lsp.protocol.make_client_capabilities(),
-    root_markers = {
-      ".git"
-    }
-  }
-)
-
-_G.vim.lsp.enable(
-  {
-    "clojure",
-    "fennel",
-    "go",
-    "lua",
-    "python",
-    "typescript",
-    "typos",
-    "zig"
-  }
-)
-
-local keymaps = {
-  { "n", "<leader>df", _G.vim.diagnostic.open_float, { desc = "Diagnostics Float" } },
-  { "n", "<leader>dn", _G.vim.diagnostic.get_next, { desc = "Diagnostics: next issue" } },
-  { "n", "<leader>dp", _G.vim.diagnostic.get_prev, { desc = "Diagnostics: prev issue" } },
-  { "n", "<leader>ds", _G.vim.diagnostic.show, { desc = "Diagnostics Show" } },
-  { "n", "<leader>ld", _G.vim.lsp.buf.definition, { desc = "LSP Definition" } },
-  { "n", "<leader>li", _G.vim.lsp.buf.incoming_calls, { desc = "LSP Incoming" } },
-  { "n", "<leader>lh", _G.vim.lsp.buf.hover, { desc = "LSP Hover" } },
-  { "n", "<leader>lr", _G.vim.lsp.buf.rename, { desc = "LSP Rename" } },
-  { "n", "<leader>lR", _G.vim.lsp.buf.references, { desc = "LSP References" } },
-}
-
-for _, keymapspec in ipairs(keymaps) do
-  _G.vim.keymap.set(unpack(keymapspec))
-end
-
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', fg = "#1e1e2e" })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+-- 
+-- _G.vim.lsp.config(
+--   "*",
+--   {
+--     capabilities = _G.vim.lsp.protocol.make_client_capabilities(),
+--     root_markers = {
+--       ".git"
+--     }
+--   }
+-- )
+-- 
+-- -- _G.vim.lsp.enable(
+-- --   {
+-- --     "clojure",
+-- -- --    "fennel",
+-- -- --    "go",
+-- -- --    "lua",
+-- -- --    "python",
+-- -- --    "typescript",
+-- -- --    "typos",
+-- -- --    "zig"
+-- --   }
+-- -- )
+-- 
+-- local keymaps = {
+--   { "n", "<leader>df", _G.vim.diagnostic.open_float, { desc = "Diagnostics Float" } },
+--   { "n", "<leader>dn", _G.vim.diagnostic.get_next, { desc = "Diagnostics: next issue" } },
+--   { "n", "<leader>dp", _G.vim.diagnostic.get_prev, { desc = "Diagnostics: prev issue" } },
+--   { "n", "<leader>ds", _G.vim.diagnostic.show, { desc = "Diagnostics Show" } },
+--   -- { "n", "<leader>ld", _G.vim.lsp.buf.definition, { desc = "LSP Definition" } },
+--   -- { "n", "<leader>li", _G.vim.lsp.buf.incoming_calls, { desc = "LSP Incoming" } },
+--   -- { "n", "<leader>lh", _G.vim.lsp.buf.hover, { desc = "LSP Hover" } },
+--   -- { "n", "<leader>lr", _G.vim.lsp.buf.rename, { desc = "LSP Rename" } },
+--   -- { "n", "<leader>lR", _G.vim.lsp.buf.references, { desc = "LSP References" } },
+-- }
+-- 
+-- for _, keymapspec in ipairs(keymaps) do
+--   _G.vim.keymap.set(unpack(keymapspec))
+-- end
